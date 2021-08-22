d1372bfad4cb08cb3dbf980260aeea90290a727b
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-08-22 18:50:14 -0400

    Testing some stuff

--- Cogs/Lights.py
+++ Cogs/Lights.py
@@ -16,8 +16,16 @@ class Lights(commands.Cog):
         self.strip_length = 450
         self.pixels = neopixel.NeoPixel(board.D18, self.strip_length, pixel_order=neopixel.RGB)
 
+    async def clear(self, show=True):
+        for i in range(self.strip_length):
+            self.pixels[i] = (0, 0, 0)
+        if show:
+            self.pixels.show()
+
+
     @commands.command()
     async def lights(self, ctx):
+        await self.clear()
         for i in range(self.strip_length):
             self.pixels[i] = (255, 255, 255)
             self.pixels.show()
