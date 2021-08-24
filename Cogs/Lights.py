9795bfda941406229736b50ee1402784157f46ff
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-08-24 15:38:42 -0400

    Live dev

--- Cogs/Lights.py
+++ Cogs/Lights.py
@@ -16,23 +16,21 @@ class Lights(commands.Cog):
         self.pixels = neopixel.NeoPixel(board.D18, self.strip_length, pixel_order=neopixel.RGB)
 
         self.current_effect = self.solid
-        self.args = []
         self.kwargs = {'r': 0, 'g': 0, 'b': 0}
 
         while True:
-            new_frame = self.current_effect(args=self.args, kwargs=self.kwargs)
+            new_frame = self.current_effect(self.args, kwargs=self.kwargs)
             for i, pixel in enumerate(new_frame):
                 self.pixels[i] = pixel
             self.pixels.show()
 
     @commands.command()
-    async def lights(self, ctx, func_name, *args, **kwargs):
+    async def lights(self, ctx, func_name, **kwargs):
         self.current_effect = getattr(self, func_name)
-        self.args = args
         self.kwargs = kwargs
-        await ctx.send(func_name + ' | ' + str(args) + ' | ' + str(kwargs))
+        await ctx.send(func_name + ' | ' + str(kwargs))
 
-    def solid(self, *args, **kwargs):
+    def solid(self, **kwargs):
         r = kwargs['r']
         g = kwargs['g']
         b = kwargs['b']
